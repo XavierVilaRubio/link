eb0f41f05401d0ab734918fe5ed1195341a337c3
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2023-01-01 19:28:20 +0100

    set colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundLight: "#fef9fb",
+        backgroundDark: "#1c2c4e",
+        textLight: "#1c2c4e",
+        textDark: "#f2f2f4",
+        accentLight: "#007c7c",
+        accentDark: "#00afaf",
+      },
+    },
   },
   plugins: [],
 };
